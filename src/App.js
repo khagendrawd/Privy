e6a5aabb5bc8d8dced3810984f3ac729ace775e4
Khagendra Singh <khagendra.singh@yatra.com>
2018-07-23 15:42:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Yatra Privy
+        <Header/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
