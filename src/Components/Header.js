c15bd0e82b6e68fbc944c8accf4b6af3631e46bc
Khagendra Singh <khagendra.singh@yatra.com>
2018-07-23 18:52:34 +0530

    header and footer update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import privy_logo from '../asset/privy_logo.png';
 
 class Header extends Component {
   render() {
     return (
       <header className="Header">
-        Privy Header
+        <div className="hcontainer">
+        <img src={privy_logo} className="privy_logo" alt="Privy Logo" />
+        </div>
       </header>
     );
   }
