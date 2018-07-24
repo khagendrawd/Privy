15132fc062ff8d524e90ed6536d4ab94c53b4990
abhishekjakhar <abhishekjakhar.11@gmail.com>
2018-07-24 22:18:39 +0530

    Code Improved + CSS Modules Added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import yatraLogo from '../../assets/images/yatra_logo.svg';
 
-import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
+import classes from './Footer.css';
 
 const footer = () => (
-    <footer className="Footer">
-        <div className="fcontainer">
-        <div className="footer_left">
-        <img src={yatraLogo} className="yatra_logo" alt="Yatra Logo" />
-        <a href="term_condition.html">Terms & Conditions</a>
-        <a href="privacy_policy">Privacy Policy</a>
+    <footer className={classes.Footer}>
+        <div className={classes.Container}>
+        <div className={classes.FooterLeft}>
+          <img src={yatraLogo} className={classes.YatraLogo} alt="Yatra Logo" />
+          <a href="term_condition.html">Terms & Conditions</a>
+          <a href="privacy_policy">Privacy Policy</a>
         </div>
-        <div className="footer_right">
+        <div className={classes.FooterRight}>
             <a href="https://www.instagram.com/yatradotcom/?hl=en" target="_blank" rel="Yatra Instagram" className="instagram">Instagram</a>
             <a href="https://twitter.com/yatraofficial?lang=en" target="_blank" rel="Yatra Twitter" className="twitter">Twitter</a>
             <a href="https://www.facebook.com/Yatra/" target="_blank" rel="Yatra Facebook" className="facebook">Facebook</a>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import privyLogo from '../../assets/images/privy_logo.png';
 
+import classes from './Header.css';
+
 const header = () => (
-  <header className="Header">
-    <div className="hcontainer">
+  <header className={classes.Header}>
+    <div className={classes.HeaderContainer}>
       <img src={privyLogo} className="privy_logo" alt="Privy Logo" />
-      <div className="header_menu">
+      <div className={classes.HeaderMenu}>
         <ul>
           <li><a href="">Travel Advisor</a></li>
           <li><a href="">Pursuits</a></li>
           <li><a href="">Tours</a></li>
-          <li className="dropdown_menu">Travel Advisor
+          <li className={classes.DropDown}>Travel Advisor
             <ul>
               <li><a href="">Gallery</a></li>
               <li><a href="">Blog</a></li>
           </ul>
           </li>
           <li><a href="">About Us</a></li>
-          <li className="login"><a href="">Login</a></li>
+          <li className={classes.Login}><a href="">Login</a></li>
         </ul>
       </div>
     </div>
