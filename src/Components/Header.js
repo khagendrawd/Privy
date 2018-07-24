cdab398add7d177782dae71845a664cbb3079d72
Khagendra Singh <khagendra.singh@yatra.com>
2018-07-24 16:08:01 +0530

    header update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,21 @@ class Header extends Component {
       <header className="Header">
         <div className="hcontainer">
         <img src={privy_logo} className="privy_logo" alt="Privy Logo" />
+        <div className="header_menu">
+        <ul>
+            <li><a href="">Travel Advisor</a></li>
+            <li><a href="">Pursuits</a></li>
+            <li><a href="">Tours</a></li>
+            <li className="dropdown_menu">Travel Advisor
+            <ul>
+                <li><a href="">Gallery</a></li>
+                <li><a href="">Blog</a></li>
+            </ul>
+            </li>
+            <li><a href="">About Us</a></li>
+            <li className="login"><a href="">Login</a></li>
+        </ul>
+        </div>
         </div>
       </header>
     );
