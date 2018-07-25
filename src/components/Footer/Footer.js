a019f5e638ca08f524c59e26f0ddcadd9421671b
abhishekjakhar <abhishekjakhar.11@gmail.com>
2018-07-25 11:16:41 +0530

    Optimizations + Bug Fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import yatraLogo from '../../assets/images/yatra_logo.svg';
+import yatraLogo from '../../assets/images/yatra-logo.svg';
 
 const footer = () => (
-    <footer className='Footer'>
-        <div className='Container'>
-        <div className='FooterLeft'>
-          <img src={yatraLogo} className='YatraLogo' alt="Yatra Logo" />
-          <a href="term_condition.html">Terms & Conditions</a>
-          <a href="privacy_policy">Privacy Policy</a>
-        </div>
-        <div className='FooterRight'>
-            <a href="https://www.instagram.com/yatradotcom/?hl=en" target="_blank" rel="Yatra Instagram" className="instagram">Instagram</a>
-            <a href="https://twitter.com/yatraofficial?lang=en" target="_blank" rel="Yatra Twitter" className="twitter">Twitter</a>
-            <a href="https://www.facebook.com/Yatra/" target="_blank" rel="Yatra Facebook" className="facebook">Facebook</a>
-        </div>
+    <footer className='footer'>
+        <div className='footer__container'>
+            <div className='footer__left'>
+                <img src={yatraLogo} className='YatraLogo' alt="Yatra Logo" />
+                <a href="term_condition.html">Terms &amp; Conditions</a>
+                <a href="privacy_policy">Privacy Policy</a>
+            </div>
+            <div className='footer__right'>
+                <a href="https://www.instagram.com/yatradotcom/?hl=en" target="_blank" rel='noreferrer noopener'>Instagram</a>
+                <a href="https://twitter.com/yatraofficial?lang=en" target="_blank" rel='noreferrer noopener'>Twitter</a>
+                <a href="https://www.facebook.com/Yatra/" target="_blank" rel='noreferrer noopener'>Facebook</a>
+            </div>
         </div>
     </footer>  
 ); 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import privyLogo from '../../assets/images/privy_logo.png';
+import privyLogo from '../../assets/images/privy-logo.png';
 
 const header = () => (
-  <header className='Header'>
-    <div className='HeaderContainer'>
+  <header className='header'>
+    <div className='header__container'>
       <img src={privyLogo} className="privy_logo" alt="Privy Logo" />
-      <div className='HeaderMenu'>
+      <div className='header__menu'>
         <ul>
           <li><a href="">Travel Advisor</a></li>
           <li><a href="">Pursuits</a></li>
           <li><a href="">Tours</a></li>
-          <li className='DropDown'>Travel Advisor
+          <li className='dropdown'>Travel Advisor
             <ul>
               <li><a href="">Gallery</a></li>
               <li><a href="">Blog</a></li>
